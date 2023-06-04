e29e141bcd55c158ed0c09092aa3306536a62add
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2023-06-04 12:42:10 -0700

    Add empty project

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this is just to make sure the code-related part of the gitignore works correctly
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

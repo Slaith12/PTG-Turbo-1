a47823cb2a44d43da1f9d86ffeda3d40a30343fb
Nikolas <nikolpautov@gmail.com>
2023-06-04 22:18:51 -0700

    ruined it (session 3 done)
    get fucked ryan

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    public string newGameScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void StartLore()
+    {
+        SceneManager.LoadScene("lore");
+    }
+    public void StartTit()
+    {
+        SceneManager.LoadScene("LeTitle");
+    }
+}
